6cb801297645785b39f88396ad3be110f33f9d7e
Abdulrahman Nagi <abdulrahman.mohamed.nagi@gmail.com>
2020-09-04 16:58:49 +0200

    1st commit

---
+++ src/main/java/com/airline/manager/model/seat/SeatsGenerator.java
@@ -0,0 +1,12 @@
+package com.airline.manager.model.seat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SeatsGenerator {
+
+	public Set<Seat> createSeats(int rows, int seatsInRow, int firstRowNumber) {
+		Set<Seat> seats = new HashSet<>();
+
+	}
+}
